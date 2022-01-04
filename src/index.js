1a0b219c8a7fc8a89fd900fb274368e27bd547ab
angelina <floresangela.0707@gmail.com>
2022-01-04 18:35:27 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse(n) {
-    if (n < 0) {
-      n *= -1;
-    }
-    return (n + '').split('').reverse().join('');
-}
+  return (Math.abs(n) + "").split("").reverse().join("");
+};
